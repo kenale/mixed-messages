b40050ff49d1742c507a8bdccdfe7b312e0147c9
Kendris <kenchanD@protonmail.com>
2024-05-15 18:54:14 +0200

    This are the phrases

---
+++ javaScript.js
@@ -0,0 +1,8 @@
+// This are the phrases of this app
+const message1 = "Hoy es un lindo día";
+const message2 = "El helado de cookies es delicioso";
+const message3 = "El cielo azul inspira";
+const message4 = "Toma agua, toma agua, TOMA AGUA!!!!";
+const message5 = "Sal a caminar y despejar la mente"
+const message6 = "Feliz día del día";
+
